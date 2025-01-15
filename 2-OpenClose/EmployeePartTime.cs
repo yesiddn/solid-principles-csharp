ba0e6dbc2866f62c2f43adf63f3577498edbc5ed
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-01-15 11:47:53 -0500

    Apply open/closed principle

--- 2-OpenClose/EmployeePartTime.cs
+++ 2-OpenClose/EmployeePartTime.cs
@@ -1,14 +1,25 @@
 namespace OpenClose
 {
-  public class EmployeePartTime
+  public class EmployeePartTime : Employee
   {
-    public string Fullname { get; set; }
-    public int HoursWorked { get; set; }
-
     public EmployeePartTime(string fullname, int hoursWorked)
     {
       Fullname = fullname;
       HoursWorked = hoursWorked;
+    }
+
+    public override decimal CalculateSalaryMonthly()
+    {
+      decimal hourValue = 20000M;
+      decimal salary = hourValue * HoursWorked;
+      if (HoursWorked > 160)
+      {
+        decimal effortCompensation = 5000M;
+        int extraDays = HoursWorked - 160;
+        salary += effortCompensation * extraDays;
+      }
+
+      return salary;
     }
   }
 }
--- 2-OpenClose/Program.cs
+++ 2-OpenClose/Program.cs
@@ -1,35 +1,16 @@
 ﻿using OpenClose;
 
-CalculateSalaryMonthly(new List<object>() {
-    new EmployeeFullTime("Pepito Pérez", 160),
-    new EmployeePartTime("Manuel Lopera", 180)
+ShowSalaryMonthly(new List<Employee>() {
+  new EmployeeFullTime("Pepito Pérez", 160),
+  new EmployeePartTime("Manuel Lopera", 180),
+  new EmployeeContractor("Juan León", 160),
 });
 
 
-void CalculateSalaryMonthly(List<object> employees)
+void ShowSalaryMonthly(List<Employee> employees)
 {
   foreach (var employee in employees)
   {
-    if (employee is EmployeeFullTime)
-    {
-      decimal hourValue = 30000M;
-      EmployeeFullTime employeeFullTime = ((EmployeeFullTime)employee);
-      decimal salary = hourValue * employeeFullTime.HoursWorked;
-      Console.WriteLine($"Empleado: {employeeFullTime.Fullname}, Pago: {salary:C1} ");
-    }
-    else
-    {
-      decimal hourValue = 20000M;
-      EmployeePartTime employeePartTime = ((EmployeePartTime)employee);
-      decimal salary = hourValue * employeePartTime.HoursWorked;
-      if (employeePartTime.HoursWorked > 160)
-      {
-        decimal effortCompensation = 5000M;
-        int extraDays = employeePartTime.HoursWorked - 160;
-        salary += effortCompensation * extraDays;
-      }
-      Console.WriteLine($"Empleado: {employeePartTime.Fullname}, Pago: {salary:C1} ");
-    }
+    Console.WriteLine($"Empleado: {employee.Fullname}, Pago: {employee.CalculateSalaryMonthly():C1} ");
   }
-
 }
